b715408e98782e54e2e1556177a5300895401fde
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-11-27 22:50:13 -0300

    se crearon componentes y rutas del proyecto de blog

---
+++ proyecto-blog-React/arrancar-proyecto.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "cd C:\Program Files\MongoDB\Server\5.0\bin && mongod.exe --dbpath C:/data/db"
+start cmd.exe /k "cd C:\Users\alber\Downloads\api-rest-node\api-rest-node && npm start"
+start cmd.exe /k "cd C:\Users\alber\OneDrive\Escritorio\Proyecto-blog-react\proyecto-blog-React && npm run dev"
+start C:\Users\alber\AppData\Local\MongoDBCompass\MongoDBCompass.exe
+start C:\Users\alber\AppData\Local\Postman\Postman.exe
